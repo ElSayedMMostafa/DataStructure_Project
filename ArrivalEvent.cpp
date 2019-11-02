55661d11e7b4eb14b2160fd069bf37f899475bb9
ElSayedMMostafa <45875353+ElSayedMMostafa@users.noreply.github.com>
2019-11-02 08:57:34 +0200

    Create ArrivalEvent.cpp

---
+++ ArrivalEvent.cpp
@@ -0,0 +1,26 @@
+#include "ArrivalEvent.h"
+#include "Restaurant.h"
+
+
+ArrivalEvent::ArrivalEvent(int eTime, int oID, ORD_TYPE oType):Event(eTime, oID)
+{
+	OrdType = oType;
+}
+
+void ArrivalEvent::Execute(Restaurant* pRest)
+{
+	//This function should create an order and fills its data 
+	// Then adds it to normal, vegan, or VIP order lists that you will create in phase1
+	Order MyOrder (OrderID,OrdType);
+	switch (OrdType)
+	{
+	case TYPE_NRM: NormalOrders.InsertEnd(&MyOrder);
+		break;
+	case TYPE_VEG: VeganOrders.InsertEnd(&MyOrder);
+		break;
+	case TYPE_VIP: VIPOrders.InsertEnd(&MyOrder);
+		break;
+	default:
+		break;
+	}
+}
